d652390af5cc239ab1d6086d3103f13c704c39bb
sunilsrblr10 <sunilsrblr10@gmail.com>
2022-12-01 13:59:32 +0530

    Adding codebase

---
+++ src/main/java/com/example/StudentRegistrationWebApp/StudentRegistrationWebApp/StudentRegistrationWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentRegistrationWebApp.StudentRegistrationWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentRegistrationWebApp/StudentRegistrationWebApp/StudentRegistrationWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentRegistrationWebApp.StudentRegistrationWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
